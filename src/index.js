65c12cf1d93de615c306a91e2354b3aed11c0adc
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-08-23 21:07:54 +0600

    Using components ceate Header, Notes, Footer

--- src/index.js
+++ src/index.js
@@ -39,3 +39,68 @@ ReactDom.render(<Output />, document.getElementById('root'));
 
 
 
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <a className="navbar-brand" href="#">React Notes App</a>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Add Notes</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About Us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Help</a>
+            </li>
+          </ul>
+      </nav>
+    )
+}
+
+const Footer = () => {
+    return (
+        <p className="text-center text-muted">&copy; All Rights Reserved 2020. Developed by <a href="https://github.com/bdmostafa">Mostafa</a></p>
+    )
+}
+
+const Note = (props) => {
+    // console.log(props.title);
+    const {title, description} = props;
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+
+    )
+}
+
+const Notes = () => {
+    return (
+        <div>
+            <Note title="Note 1" description="Note 1 description"/>
+            <Note title="Note 1" description="Note 1 description" />
+            <Note title="Note 1" description="Note 1 description" />
+        </div>
+    )
+}
+
+
+const App = () => {
+    return(
+        <div className="container">
+            <Header />
+            <Notes />
+            <Footer />
+        </div>
+        
+    )
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
