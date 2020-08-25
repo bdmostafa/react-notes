bad5db0d5b24fd4e95adf2f947f851448fb521ed
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-08-25 11:15:11 +0600

    create build

--- src/contexts/Note.context.js
+++ src/contexts/Note.context.js
@@ -1,4 +1,4 @@
-import React, { Component, createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 // Functional component
